7abcb4762d40f5cc946ee97aa1ac49b697571d6b
caojp <13176666869@163.com>
2024-10-10 13:46:17 +0800

    1. 排除掉影藏目录和影藏文件 2. 排除掉非.md开头的文件

--- backend/services/file_service.go
+++ backend/services/file_service.go
@@ -6,10 +6,44 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
+//func ReadDir(dirPath string) ([]models.FileNode, error) {
+//	var fileNodes []models.FileNode
+//	entries, err := os.ReadDir(dirPath)
+//	if err != nil {
+//		logger.Error("Failed to read directory:", dirPath, "Error:", err)
+//		return nil, err
+//	}
+//
+//	for _, entry := range entries {
+//		path := filepath.Join(dirPath, entry.Name())
+//		fileNode := models.FileNode{
+//			Name:  entry.Name(),
+//			Path:  path,
+//			IsDir: entry.IsDir(),
+//		}
+//		if entry.IsDir() {
+//			children, err := ReadDir(path)
+//			if err != nil {
+//				logger.Error("Failed to read subdirectory:", path, "Error:", err)
+//				return nil, err
+//			}
+//			fileNode.Children = children
+//		}
+//		fileNodes = append(fileNodes, fileNode)
+//	}
+//
+//	logger.Info("Directory read successfully:", dirPath)
+//
+//	return fileNodes, nil
+//}
+
+// ReadDir 读取指定目录，返回 .md 文件和目录树
 func ReadDir(dirPath string) ([]models.FileNode, error) {
 	var fileNodes []models.FileNode
+
 	entries, err := os.ReadDir(dirPath)
 	if err != nil {
 		logger.Error("Failed to read directory:", dirPath, "Error:", err)
@@ -17,25 +51,39 @@ func ReadDir(dirPath string) ([]models.FileNode, error) {
 	}
 
 	for _, entry := range entries {
-		path := filepath.Join(dirPath, entry.Name())
-		fileNode := models.FileNode{
-			Name:  entry.Name(),
-			Path:  path,
-			IsDir: entry.IsDir(),
+		// 排除隐藏目录和文件
+		if strings.HasPrefix(entry.Name(), ".") {
+			continue // 跳过以 "." 开头的文件或目录
 		}
+
+		// 只处理 .md 文件或目录
+		path := filepath.Join(dirPath, entry.Name())
 		if entry.IsDir() {
+			// 递归读取子目录
 			children, err := ReadDir(path)
 			if err != nil {
 				logger.Error("Failed to read subdirectory:", path, "Error:", err)
 				return nil, err
 			}
-			fileNode.Children = children
+			fileNode := models.FileNode{
+				Name:     entry.Name(),
+				Path:     path,
+				IsDir:    true,
+				Children: children,
+			}
+			fileNodes = append(fileNodes, fileNode)
+		} else if strings.HasSuffix(entry.Name(), ".md") {
+			// 只添加 .md 文件
+			fileNode := models.FileNode{
+				Name:  entry.Name(),
+				Path:  path,
+				IsDir: false,
+			}
+			fileNodes = append(fileNodes, fileNode)
 		}
-		fileNodes = append(fileNodes, fileNode)
 	}
 
 	logger.Info("Directory read successfully:", dirPath)
-
 	return fileNodes, nil
 }
 
@@ -47,7 +95,12 @@ func ReadFile(filePath string) (string, error) {
 		logger.Error("Failed to open file:", filePath, "Error:", err)
 		return "", err
 	}
-	defer file.Close() // 确保文件在读取后被关闭
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			logger.Error("Failed to close file:", filePath, "Error:", err)
+		}
+	}(file) // 确保文件在读取后被关闭
 
 	// 使用 io.ReadAll 读取文件内容
 	content, err := io.ReadAll(file)
